08abcae5443f5c3655886ed158f7ec26d2c7a2fd
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-11-16 23:20:58 +0100

    Add testing rotating and reset logo script

---
+++ scenes/levels/level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Logo.rotation_degrees = 90
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	$Logo.rotation_degrees += 1
+
+	if $Logo.rotation_degrees > 180:
+		$Logo.rotation_degrees = 0
+		
+	# Reset logo position to x=0 again
+	if $Logo.position.x > 1000:
+		$Logo.position.x = 0
