163e8b354a8b2b509df5709351219e73782ec27e
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-11-17 22:02:39 +0100

    Add collisions

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -6,7 +6,7 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	
 	# Check if the key is pressed down
 	Input.is_action_pressed("left")
--- scenes/player/heroPlayer.gd
+++ scenes/player/heroPlayer.gd
@@ -1,16 +1,24 @@
-extends Node2D
+extends CharacterBody2D
 
+const characterSpeed: int = 500
 
-func _process(delta):
+func _process(_delta):
 	
-	# Top-down movement
+	# Get input direction for top-down movement
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
 	
-	# Shoot Laser
+	# Calculate velocity based on input direction and character speed
+	# The _process delta variable is automatically considered for smooth 
+	# movement
+	velocity = direction * characterSpeed
+	
+	# Move the player based on the calculated velocity
+	move_and_slide()
+	
+	# Check if the primary action button is pressed to shoot a laser
 	if Input.is_action_pressed("primaryAction"):
 		print("Shoot Laser!")
 
-	# Shoot Grenade
+	# Check if the secondary action button is pressed to shoot a grenade
 	if Input.is_action_pressed("secondaryAction"):
 		print("Shoot Grenade!")
