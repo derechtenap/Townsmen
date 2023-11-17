aed6f2e4f29929063ea84b05e74a6a2f5e89e4b2
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2023-11-17 22:04:19 +0100

    Removed legacy code

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,12 +1,1 @@
 extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	
-	# Check if the key is pressed down
-	Input.is_action_pressed("left")
